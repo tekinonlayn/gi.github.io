fcc83c37adade87fbb9e44fa68d92f610f4a9dac
Gültekin IŞIK <tekinler@gmail.com>
2016-04-07 05:26:42 +0300

    Update quotes.js

--- js/quotes.js
+++ js/quotes.js
@@ -1,1 +1,10 @@
-var quotes = [];
+var quotes = [[]];
+
+quotes[0] = ["ak akçe kara gün", "ali"];
+quotes[1] = ["sakla samanı", "veli"];
+quotes[2] = ["bilgisayar", "cem"];
+quotes[3] = ["mühendisi", "can"];
+
+var rand = Math.floor(Math.random()* 10 * quotes.length) % quotes.length;
+
+document.getElementById("pid").innerHTML = quotes[rand][0] + "<p><b>"+ "- " + quotes[rand][1]+"</b></p>";

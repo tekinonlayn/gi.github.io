f762d3c2b956f02fd4da44c9ee45da463bca93cd
Gültekin IŞIK <tekinler@gmail.com>
2016-04-07 05:39:09 +0300

    Update quotes.js

--- js/quotes.js
+++ js/quotes.js
@@ -1,9 +1,13 @@
 var quotes = [[]];
 
-quotes[0] = ["Hoşgeldiniz! Kendinize güvenin! Akranlarınızın, çağınızın, Ezelî ve Ebedî Gerçek’ten payınıza düşenin hakkını verin. Dil, din, ırk, cinsiyet ayrımının tuzağına düşmeden, zamanınızın en yetkin bilginleriyle, sanatçı ve filozoflarıyla dostluk kurun. Mahrem düşüncelerinizi aşkın zekalarla paylaşın.", "Uygur Diskuru"];
-quotes[1] = ["sakla samanı", "veli"];
-quotes[2] = ["bilgisayar", "cem"];
-quotes[3] = ["mühendisi", "can"];
+quotes[0] = ["Hoşgeldiniz! Kendinize güvenin! Akranlarınızın," + 
+" çağınızın, Ezelî ve Ebedî Gerçek’ten payınıza düşenin hakkını" + 
+" verin. Dil, din, ırk, cinsiyet ayrımının tuzağına düşmeden," + 
+" zamanınızın en yetkin bilginleriyle, sanatçı ve filozoflarıyla" + 
+" dostluk kurun. Mahrem düşüncelerinizi aşkın zekalarla paylaşın.", "Uygur Diskuru"];
+quotes[1] = ["boş", "boş"];
+quotes[2] = ["boş", "boş"];
+quotes[3] = ["boş", "boş"];
 
 var rand = Math.floor(Math.random()* 10 * quotes.length) % quotes.length;
 
